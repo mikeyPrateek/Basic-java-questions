3fd2c3ea6d2892da525e233ee81572c216589a89
MikeyPrateek <137617537+mikeyPrateek@users.noreply.github.com>
2024-08-14 02:20:35 +0000

    4 ways of swaping numbers

--- swap_5_ways.java
+++ swap_5_ways.java
@@ -4,6 +4,36 @@
 public class swap_5_ways {
 
     public static void main(String[] args) {
-        System.out.println("Hello");
+        int a=10 , b=20;
+        System.out.println("Before the swapping the number " + a +" ," + b);
+
+        // Logic_1  // use 3rd variable as temp.     
+        // int temp = a;
+        // a = b;
+        // b = temp;
+
+        // Logic_2 // use operators + , -  
+
+        // a= a+b;    10+20=30
+        // b= a-b;    30-20=10
+        // a= a-b;    30-50=20
+
+        // Logic_2 // use operators + , -  
+
+        // a= a*b;   
+        // b= a/b;   
+        // a= a/b;   
+
+
+        // Logic_4 // use bitwise operator Xor 00=0 10=1 01=1 11=0
+
+     /* a = a^b; // this will convert decimal to binary 
+        b = a^b;
+        a = a^b; */
+ 
+        System.out.println("After the swapping the number " + a +" ," + b);
     }
 }
+
+
+
